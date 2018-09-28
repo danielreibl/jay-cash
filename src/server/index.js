03e94616050e5570c2d8bb42d7f496d28f9a457a
danielreibl <daniel.reibl@guidesmiths.com>
2018-09-28 12:13:10 +0200

    Add bet endpoint and express error handler

--- src/server/index.js
+++ src/server/index.js
@@ -1,14 +1,18 @@
 import express from 'express';
 
 const axios = require('axios');
+const bodyParser = require('body-parser');
 
 import html from '../client/components/html-template';
 
 const app = express();
 const port = 5000;
 const TARGET_URL = 'http://hackathon.guidesmiths.com:4000';
+const GAME_ID = 'jay-cash';
 
 app.get('/', (req, res) => res.send(html));
+
+app.use(bodyParser.json());
 
 app.get('/api/user/:userName', async (req, res) => {
   const userResponse = await axios.post(`${TARGET_URL}/api/user`, {
@@ -18,12 +22,23 @@ app.get('/api/user/:userName', async (req, res) => {
   return res.json(userResponse.data);
 });
 
-app.get('/api/state/:userId', (req, res) => res.json({
-  _id: '5baa2c26533d6fb80f51c00c',
-  name: 'kunb',
-  displayName: 'Kun Bela',
-  coin: 1000
-}));
+app.post('/api/bet', async (req, res) => {
+  const { userName, bet, change } = req.body;
+  const betResponse = await axios.post(`${TARGET_URL}/api/nav/log`, {
+    bet,
+    change,
+    userName,
+    gameId: GAME_ID,
+  });
+
+  return res.json(betResponse.data);
+});
+
 app.use(express.static('dist'));
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  return res.status(500);
+});
 
 app.listen(port, '0.0.0.0', () => console.log(`JayCash is listening on port ${port}!`));
