c66d4294efc9f9f197f4cf425d51a431191c08ac
danielreibl <daniel.reibl@guidesmiths.com>
2018-09-28 11:40:30 +0200

    Get user endpoint

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,24 @@
 import express from 'express';
+
+const axios = require('axios');
+
 import html from '../client/components/html-template';
 
 const app = express();
 const port = 5000;
+const TARGET_URL = 'http://hackathon.guidesmiths.com:4000';
 
 app.get('/', (req, res) => res.send(html));
-app.get('/api/state', (req, res) => res.json({
+
+app.get('/api/user/:userName', async (req, res) => {
+  const userResponse = await axios.post(`${TARGET_URL}/api/user`, {
+    name: req.params.userName,
+  });
+
+  return res.json(userResponse.data);
+});
+
+app.get('/api/state/:userId', (req, res) => res.json({
   _id: '5baa2c26533d6fb80f51c00c',
   name: 'kunb',
   displayName: 'Kun Bela',
