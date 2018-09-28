af60303dc650f1ea107d89072a639c3fb7bfda22
Robert Lovei <loveirobert@gmail.com>
2018-09-28 15:00:54 +0200

    sounds

--- src/client/components/roulette.js
+++ src/client/components/roulette.js
@@ -104,6 +104,8 @@ export default class Roulette extends Component {
     window.Rgm = RouletteSpin
     return (
       <div>
+        <audio src={"/assets/sounds/hall.mp3"} controls="true" loop="true" autoplay="true"></audio>
+        {!this.state.canSpin ? <audio src={!this.state.canSpin ? "/assets/sounds/ball.mp3" : ""} controls="true" loop="true" autoplay="true"></audio> : null}
         <main>
           <div id="roulette">
             <div class="header">
